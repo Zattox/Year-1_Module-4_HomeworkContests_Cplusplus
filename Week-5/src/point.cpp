91702519578c06aa80103dc1700e0a698dd86f82
Zattox <vladcsgoogurechik@mail.ru>
2024-05-22 16:21:24 +0300

    Исправил ошибки сборки файлов (забыл добавить файл point.cpp)

--- Week-5/src/point.cpp
+++ Week-5/src/point.cpp
@@ -1,59 +1,79 @@
 #include "../point.h"
-#include "../vector.h"
+#include "vector.cpp"
 
-using Geometry::Vector, Geometry::Point, Geometry::IShape;
+using namespace Geometry;
 
-Point::Point() : point_(0, 0) {}
+Point::Point() : x_(0), y_(0) {}
 
-Point::Point(int64_t x, int64_t y) : point_(x, y) {}
+Point::Point(int64_t x, int64_t y) : x_(x), y_(y) {}
 
-Point::Point(const Point &point) : point_(point.point_) {}
+Point::Point(const Point &point) : x_(point.GetX()), y_(point.GetY()) {}
 
-Point::Point(const Vector &vector) : point_(vector) {}
+Point::Point(const Vector &vector) : x_(vector.GetX()), y_(vector.GetX()){}
 
-Point &Point::operator=(const Point &point) = default;
-
-bool Point::operator==(const Point &vector) const {
-  return (point_ == vector);
+bool Point::operator==(const Point &point) const {
+  return (x_ == point.GetX()) && (y_ == point.GetY());
 }
 
 bool Point::operator!=(const Point &vector) const {
-  return (point_ != vector);
+  return !(*this == vector);
 }
 
-Vector Point::GetVec() const {
-  return point_;
+void Point::SetX(const int64_t &value) {
+  x_ = value;
+}
+
+void Point::SetY(const int64_t &value) {
+  y_ = value;
+}
+
+int64_t Point::GetX() const {
+  return x_;
+}
+
+int64_t Point::GetY() const {
+  return y_;
+}
+
+IShape &Geometry::Point::Move(const Vector &vector) {
+  x_ += vector.GetX();
+  y_ += vector.GetY();
+  return *this;
 }
 
 bool Point::ContainsPoint(const Point &point) const {
   return point == *this;
 }
 
-bool Point::CrossesSegment(const Geometry::Segment &segment) const {
+/*bool Point::CrossesSegment(const Geometry::Segment &segment) const {
   return segment.ContainsPoint(*this);
+}*/
+
+IShape *Point::Clone() const {
+  IShape *it = new Point(*this);
+  return it;
 }
 
 std::string Point::ToString() const {
   std::string text = "Point(";
-  text += std::to_string(point_.GetX());
+  text += std::to_string(x_);
   text += ',';
-  text += std::to_string(point_.GetY());
+  text += std::to_string(y_);
   text += ')';
   return text;
 }
 
-std::ostream &Geometry::operator<<(std::ostream &out, const Point &value) {
+std::ostream &operator<<(std::ostream &out, const Point &value) {
   out << value.ToString();
   return out;
 }
 
-std::istream &Geometry::operator>>(std::istream &in, Point &value) {
+std::istream &operator>>(std::istream &in, Point &value) {
   int64_t first_num, second_num;
   in >> first_num >> second_num;
   if (in) {
-    value.point_.x_ = first_num;
-    value.point_.y_ = second_num;
+    value.SetX(first_num);
+    value.SetY(second_num);
   }
   return in;
 }
-
