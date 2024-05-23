8259e4f91c6f9468328824b265fecd61ff393cce
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-05-24 01:47:06 +0300

    Реализовал класс Circle

--- Week-5/circle.h
+++ Week-5/circle.h
@@ -1,4 +1,37 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
+#include "IShape.h"
+#include "point.h"
+#include "line.h"
+#include "vector.h"
+
+namespace geometry {
+
+class Circle : public IShape {
+ public:
+  Circle();
+  Circle(const Point &centre, const int64_t &radius);
+  Circle(const Circle &circle);
+
+  void SetCentre(const Point &point);
+  void SetRadius(const int64_t &radius);
+  [[nodiscard]] Point GetCentre() const;
+  [[nodiscard]] int64_t GetRadius() const;
+
+  [[nodiscard]] IShape &Move(const Vector &vector) override;
+  [[nodiscard]] bool ContainsPoint(const Point &point) const override;
+  [[nodiscard]] bool CrossesSegment(const Segment &segment) const override;
+  [[nodiscard]] IShape *Clone() const override;
+  [[nodiscard]] std::string ToString() const override;
+
+  friend std::ostream &operator<<(std::ostream &out, const Circle &value);
+
+ private:
+  Point centre_;
+  int64_t radius_;
+};
+
+}
+
 #endif // CIRCLE_H
--- Week-5/src/circle.cpp
+++ Week-5/src/circle.cpp
@@ -1,3 +1,66 @@
-//
-// Created by dumpl on 21.05.2024.
-//
+#include "../circle.h"
+
+namespace geometry {
+
+Circle::Circle() : centre_(Point()), radius_(0) {}
+
+Circle::Circle(const Point &centre, const int64_t &radius) : centre_(centre), radius_(radius) {}
+
+Circle::Circle(const Circle &circle) : centre_(circle.GetCentre()), radius_(circle.GetRadius()) {}
+
+void Circle::SetCentre(const Point &point) {
+  centre_ = point;
+}
+
+void Circle::SetRadius(const int64_t &radius) {
+  radius_ = radius;
+}
+
+Point Circle::GetCentre() const {
+  return centre_;
+}
+
+int64_t Circle::GetRadius() const {
+  return radius_;
+}
+
+IShape &Circle::Move(const Vector &vector) {
+  centre_.SetX(centre_.GetX() + vector.GetX());
+  centre_.SetY(centre_.GetY() + vector.GetY());
+  return *this;
+}
+
+bool Circle::ContainsPoint(const Point &point) const {
+  Vector vector = point - centre_;
+  return vector.Length() <= radius_;
+}
+
+bool Circle::CrossesSegment(const Segment &segment) const {
+  Vector vector_start = segment.GetStart() - centre_;
+  Vector vector_end = segment.GetEnd() - centre_;
+  bool flag1 = ((vector_start.Length() <= radius_) && (vector_end.Length() >= radius_));
+  bool flag2 = ((vector_start.Length() >= radius_) && (vector_end.Length() <= radius_));
+
+  return flag1 || flag2;
+}
+
+IShape *Circle::Clone() const {
+  IShape *it = new Circle(*this);
+  return it;
+}
+
+std::string Circle::ToString() const {
+  std::string text = "Circle(";
+  text += centre_.ToString();
+  text += ", ";
+  text += std::to_string(radius_);
+  text += ')';
+  return text;
+}
+
+std::ostream &operator<<(std::ostream &out, const Circle &value) {
+  out << value.ToString();
+  return out;
+}
+
+}
