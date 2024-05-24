8e4c336cbf43fa812651091353c5db193cd3cf59
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-05-24 03:06:43 +0300

    Переделал Circle::CrossesSegment

--- Week-5/src/circle.cpp
+++ Week-5/src/circle.cpp
@@ -39,12 +39,22 @@ bool Circle::ContainsPoint(const Point &point) const {
 }
 
 bool Circle::CrossesSegment(const Segment &segment) const {
-  Vector vector_start = segment.GetStart() - centre_;
-  Vector vector_end = segment.GetEnd() - centre_;
-  bool flag1 = ((vector_start.Length() <= radius_) && (vector_end.Length() >= radius_));
-  bool flag2 = ((vector_start.Length() >= radius_) && (vector_end.Length() <= radius_));
+  Vector start = {segment.GetStart().GetX(), segment.GetStart().GetY()};
+  Vector end = {segment.GetEnd().GetX(), segment.GetEnd().GetY()};
+  Vector centre = {centre_.GetX(), centre_.GetY()};
 
-  return flag1 || flag2;
+  long double distance;
+  if (DotProduct(centre - start, end - start) >= 0 && DotProduct(centre - end, start - end) >= 0) {
+    distance = std::abs(CrossProduct(centre - start, centre - end)) / (start - end).Length();
+  } else {
+    distance = std::min((centre - start).Length(), (centre - end).Length());
+  }
+
+  if ((start - centre).Length() == radius_ || (end - centre).Length() == radius_) {
+    return true;
+  }
+
+  return distance <= radius_ && !(ContainsPoint(segment.GetStart()) && ContainsPoint(segment.GetEnd()));
 }
 
 IShape *Circle::Clone() const {
